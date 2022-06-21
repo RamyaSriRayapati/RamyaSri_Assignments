8e76a5bcd97b6d78f5aa2e14a9ca568748daa989
RamyaSriRayapati <47748434+RamyaSriRayapati@users.noreply.github.com>
2022-06-21 10:32:08 +0530

    committed

---
+++ rest/Restfulwebservices/src/test/java/com/teamb/Restfulwebservices/RestfulwebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamb.Restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulwebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
