24af1e4d9c722e8530d64a59efd42a6d093865ed
RamyaSriRayapati <47748434+RamyaSriRayapati@users.noreply.github.com>
2022-06-21 10:06:11 +0530

    committed

---
+++ Java11/src/fourthquestion/Read.java
@@ -0,0 +1,21 @@
+package fourthquestion;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.function.Predicate;
+
+public class Read {
+
+	public static void main(String[] args) throws IOException {
+		var paths="C:\\Users\\RRAMYASR\\Downloads\\Java11Assignments_StudentList.txt";
+
+		System.out.println("All the Students in the list are: ");
+		Files.lines(Path.of(paths)).filter(Predicate.not(String::isEmpty)).forEach(System.out::println);
+
+		int x = (int)Files.lines(Path.of(paths)).filter(Predicate.not(String::isEmpty)).count();
+		System.out.println("The numbers of students in the sheet is: "+x);
+
+	}
+
+}
